c81d8d387fd4f8dc0abc3fca64888a73672c8df9
samwoodworth <swoodworth177@gmail.com>
2022-01-20 02:09:55 -0500

    fixing value insertion

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO issue(id, issue, creatorName) VALUES (nextval('hibernate_sequence'), 'Cherry', 'Sam Woodworth');
+INSERT INTO issue(id, issue, creatorName) VALUES (nextval('hibernate_sequence'), 'Apple', 'Sam Woodworth');
+INSERT INTO issue(id, issue, creatorName) VALUES (nextval('hibernate_sequence'), 'Banana', 'Sam Woodworth');
