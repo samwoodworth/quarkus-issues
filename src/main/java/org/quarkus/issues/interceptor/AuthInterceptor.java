64c485aa0c9867e8c876f94668d30677c2df0b6f
samwoodworth <swoodworth177@gmail.com>
2022-02-03 14:11:22 -0500

    getting auth status

--- src/main/java/org/quarkus/issues/interceptor/AuthInterceptor.java
+++ src/main/java/org/quarkus/issues/interceptor/AuthInterceptor.java
@@ -1,15 +1,44 @@
 package org.quarkus.issues.interceptor;
 
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 
 @Provider
 public class AuthInterceptor implements ContainerRequestFilter {
 
+    @Context
+    HttpServerRequest req;
+    @Context
+    HttpServerResponse res;
+
     @Override
-    public void filter(ContainerRequestContext requestContext) {
-        System.out.println(requestContext.getMethod());
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+
+        String responseBody;
+        String username = req.getParam("user");
+        System.out.println("Param is: " + username);
+
+        if (username != null) {
+            URLConnection con = new URL("http://localhost:8081/isAuth?user=" + username).openConnection();
+            InputStream inputStream = con.getInputStream();
+
+            try (Scanner scanner = new Scanner(inputStream)) {
+                responseBody = scanner.useDelimiter("\\A").next();
+            }
+
+            System.out.println("Responsebody is: " + responseBody);
+        }
+
     }
 }
