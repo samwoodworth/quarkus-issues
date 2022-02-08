1c0564be361ffb4a6dd098cf33f5da1953fcb585
samwoodworth <swoodworth177@gmail.com>
2022-02-08 14:14:15 -0500

    changing data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO issue(id, issue, creatorName) VALUES (nextval('hibernate_sequence'), 'Cherry', 'Sam Woodworth');
-INSERT INTO issue(id, issue, creatorName) VALUES (nextval('hibernate_sequence'), 'Apple', 'Sam Woodworth');
-INSERT INTO issue(id, issue, creatorName) VALUES (nextval('hibernate_sequence'), 'Banana', 'Sam Woodworth');
+INSERT INTO issue(id, issue, creatorName) VALUES (nextval('hibernate_sequence'), 'Issue 1', 'Sam Woodworth1');
+INSERT INTO issue(id, issue, creatorName) VALUES (nextval('hibernate_sequence'), 'Issue 2', 'Sam Woodworth2');
+INSERT INTO issue(id, issue, creatorName) VALUES (nextval('hibernate_sequence'), 'Issue 3', 'Sam Woodworth3');
